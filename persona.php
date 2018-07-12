61729649b5ec1e72faeb7a4eae27192bba8dff6a
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-12 09:27:48 -0600

    Add files via upload
    Added files manually via upload

---
+++ persona.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Persona Profile</title>
+		<meta charset="UTF-8" />
+	<body>
+		<h1>Ken James</h1>
+		<div>
+			<img src="./ken.jpg" alt="Ken James">
+			<p>Ken is a 25 year old software engineer. He is single, lives with his girlfriend in an apartment.</p>
+		</div>
+		<div>
+			<p>Ken is tech-savvy. He is very proficient with all recent technologies. Ken owns a Windows Surface Pro 12.3 laptop (Core i7, 7300U, Windows 10; 2736x1824px), an Apple iPad Pro A10X (Chip - 256GB; 12.9-inch display with 2732x2048px resolution and 5.6 million pixels), and an iPhone (Apple iPhone 8 Plus 64GB; 1136x640-pixel resolution).</p>
+		</div>
+		<div>
+			<p>Ken especially interested in web development. He likes to attend various conferences and talks on recent advances and trends in modern web development. He wants to keep his software development skills up to date, and be fluent with all modern techniques and technologies in his field of work.</p>
+		</div>
+		<div>
+			<p>At the same time, Ken is a huge fan of rock music and avid concert goers. When he doesn’t have an opportunity to go to the rock music concert, he visits Epik Music Videos website and reads articles and blogs on it (www.epikmusicvideos.com).</p>
+		</div>
+		<div>
+			<p>What really frustrates Ken is when his favorite site doesn’t have recent music news.</p>
+		</div>
+	</body>
+</html>
